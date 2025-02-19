247b63b902910970cb7c6ff26b7489d3365948b9
kaneeldias <kaneeldias@hotmail.com>
2025-02-19 23:00:29 +0530

    Add error handler tests for various error scenarios

--- _test/test_utils.go
+++ _test/test_utils.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"bytes"
 	"github.com/gin-gonic/gin"
 	"net/http/httptest"
 )
@@ -12,4 +13,24 @@ func GenerateBasicRequest() *gin.Context {
 
 	c.Request = httptest.NewRequest("POST", "/challenges", nil)
 	return c
+}
+
+type BodyLogWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w BodyLogWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
+
+func (w BodyLogWriter) GetBody() string {
+	return w.body.String()
+}
+
+func AttachBodyLogWriter(c *gin.Context) *BodyLogWriter {
+	blw := &BodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
+	c.Writer = blw
+	return blw
 }
---
+++ middleware/error_handler_test.go
@@ -0,0 +1,110 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+	test "the-wedding-game-api/_test"
+	apperrors "the-wedding-game-api/errors"
+)
+
+func TestErrorHandlerWithNoError(t *testing.T) {
+	request := test.GenerateBasicRequest()
+	ErrorHandler(request)
+}
+
+func TestErrorHandlerWithAccessTokenNotFoundError(t *testing.T) {
+	request := test.GenerateBasicRequest()
+	blw := test.AttachBodyLogWriter(request)
+	_ = request.Error(apperrors.NewAccessTokenNotFoundError())
+	ErrorHandler(request)
+
+	if request.Writer.Status() != http.StatusForbidden {
+		t.Errorf("expected 403 but got %d", request.Writer.Status())
+	}
+
+	expectedBody := "{\"message\":\"access token not found\",\"status\":\"error\"}"
+	if blw.GetBody() != expectedBody {
+		t.Errorf("expected %s but got %s", expectedBody, blw.GetBody())
+	}
+}
+
+func TestErrorHandlerWithAuthenticationError(t *testing.T) {
+	request := test.GenerateBasicRequest()
+	blw := test.AttachBodyLogWriter(request)
+	_ = request.Error(apperrors.NewAuthenticationError("test_error"))
+	ErrorHandler(request)
+
+	if request.Writer.Status() != http.StatusForbidden {
+		t.Errorf("expected 403 but got %d", request.Writer.Status())
+	}
+
+	expectedBody := "{\"message\":\"test_error\",\"status\":\"error\"}"
+	if blw.GetBody() != expectedBody {
+		t.Errorf("expected %s but got %s", expectedBody, blw.GetBody())
+	}
+}
+
+func TestErrorHandlerWithAuthorizationError(t *testing.T) {
+	request := test.GenerateBasicRequest()
+	blw := test.AttachBodyLogWriter(request)
+	_ = request.Error(apperrors.NewAuthorizationError())
+	ErrorHandler(request)
+
+	if request.Writer.Status() != http.StatusForbidden {
+		t.Errorf("expected 403 but got %d", request.Writer.Status())
+	}
+
+	expectedBody := "{\"message\":\"access denied\",\"status\":\"error\"}"
+	if blw.GetBody() != expectedBody {
+		t.Errorf("expected %s but got %s", expectedBody, blw.GetBody())
+	}
+}
+
+func TestErrorHandlerWithValidationError(t *testing.T) {
+	request := test.GenerateBasicRequest()
+	blw := test.AttachBodyLogWriter(request)
+	_ = request.Error(apperrors.NewValidationError("test_error"))
+	ErrorHandler(request)
+
+	if request.Writer.Status() != http.StatusBadRequest {
+		t.Errorf("expected 400 but got %d", request.Writer.Status())
+	}
+
+	expectedBody := "{\"message\":\"test_error\",\"status\":\"error\"}"
+	if blw.GetBody() != expectedBody {
+		t.Errorf("expected %s but got %s", expectedBody, blw.GetBody())
+	}
+}
+
+func TestErrorHandlerWithNotFoundError(t *testing.T) {
+	request := test.GenerateBasicRequest()
+	blw := test.AttachBodyLogWriter(request)
+	_ = request.Error(apperrors.NewNotFoundError("test_entity", "test_key"))
+	ErrorHandler(request)
+
+	if request.Writer.Status() != http.StatusNotFound {
+		t.Errorf("expected 404 but got %d", request.Writer.Status())
+	}
+
+	expectedBody := "{\"message\":\"test_entity with key test_key not found.\",\"status\":\"error\"}"
+	if blw.GetBody() != expectedBody {
+		t.Errorf("expected %s but got %s", expectedBody, blw.GetBody())
+	}
+}
+
+func TestErrorHandlerWithUnexpectedError(t *testing.T) {
+	request := test.GenerateBasicRequest()
+	blw := test.AttachBodyLogWriter(request)
+	_ = request.Error(errors.New("unexpected error"))
+	ErrorHandler(request)
+
+	if request.Writer.Status() != http.StatusInternalServerError {
+		t.Errorf("expected 500 but got %d", request.Writer.Status())
+	}
+
+	expectedBody := "{\"message\":\"An unexpected error occurred.\",\"status\":\"error\"}"
+	if blw.GetBody() != expectedBody {
+		t.Errorf("expected %s but got %s", expectedBody, blw.GetBody())
+	}
+}
