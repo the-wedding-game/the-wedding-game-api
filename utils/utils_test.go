55efaf1d5af00f2e54da3272bfee307290bc9b66
kaneeldias <kaneeldias@hotmail.com>
2025-02-19 12:31:40 +0530

    Add test case for IsUrlStrict util function

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -39,3 +39,37 @@ func TestIsChallengeInSubmissionsWhenNotExists(t *testing.T) {
 	assert.Equal(t, IsChallengeInSubmissions(8, submissions), false)
 	assert.Equal(t, IsChallengeInSubmissions(324, submissions), false)
 }
+
+func TestIsURLStrictWithValidUrls(t *testing.T) {
+	assert.Equal(t, IsURLStrict("http://www.google.com"), true)
+	assert.Equal(t, IsURLStrict("https://www.google.com"), true)
+	assert.Equal(t, IsURLStrict("http://google.com"), true)
+	assert.Equal(t, IsURLStrict("https://google.com"), true)
+	assert.Equal(t, IsURLStrict("http://www.google.com/"), true)
+	assert.Equal(t, IsURLStrict("https://www.google.com/"), true)
+	assert.Equal(t, IsURLStrict("http://google.com/"), true)
+	assert.Equal(t, IsURLStrict("https://google.com/"), true)
+	assert.Equal(t, IsURLStrict("http://www.google.com/path"), true)
+	assert.Equal(t, IsURLStrict("https://www.google.com/path"), true)
+	assert.Equal(t, IsURLStrict("http://google.com/path"), true)
+	assert.Equal(t, IsURLStrict("https://google.com/path"), true)
+	assert.Equal(t, IsURLStrict("https://google.com/path/image.jpg"), true)
+	assert.Equal(t, IsURLStrict("https://google.net/path/image.jpg"), true)
+	assert.Equal(t, IsURLStrict("https://subdomain.google.net/path/image.jpg"), true)
+}
+
+func TestIsURLStrictWithInvalidUrls(t *testing.T) {
+	assert.Equal(t, IsURLStrict("www.google.com"), false)
+	assert.Equal(t, IsURLStrict("google.com"), false)
+	assert.Equal(t, IsURLStrict("www.google.com/"), false)
+	assert.Equal(t, IsURLStrict("google.com/"), false)
+	assert.Equal(t, IsURLStrict("www.google.com/path"), false)
+	assert.Equal(t, IsURLStrict("google.com/path"), false)
+	assert.Equal(t, IsURLStrict("google.com/path/image.jpg"), false)
+	assert.Equal(t, IsURLStrict("google.net/path/image.jpg"), false)
+	assert.Equal(t, IsURLStrict("subdomain.google.net/path/image.jpg"), false)
+	assert.Equal(t, IsURLStrict("http://"), false)
+	assert.Equal(t, IsURLStrict("https://"), false)
+	assert.Equal(t, IsURLStrict("https://invalid"), false)
+	assert.Equal(t, IsURLStrict("www.google.com"), false)
+}
