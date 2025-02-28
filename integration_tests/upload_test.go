ef8325eacb35a3f627f2789141677c159f4a9a61
kaneeldias <kaneeldias@hotmail.com>
2025-02-28 22:41:23 +0530

    Enable Docker Compose down in TearDown function and remove Debug from AutoMigrate call

--- integration_tests/upload_test.go
+++ integration_tests/upload_test.go
@@ -56,19 +56,41 @@ func TestUploadImage(t *testing.T) {
 func TestUploadImageNotAdmin(t *testing.T) {
 	_, accessToken, err := createUserAndGetAccessToken()
 	if err != nil {
-		t.Errorf("Error creating user and getting access token")
+		t.Errorf("Error creating admin and getting access token")
 		return
 	}
 
 	statusCode, body := makeRequestWithFile("POST", "/upload", "image", "../_tests/assets/test_upload_image.jpg", accessToken.Token)
-	if statusCode != http.StatusForbidden {
-		t.Errorf("Expected status code 403, got %v", statusCode)
+	if statusCode != http.StatusOK {
+		t.Errorf("Expected status code 200, got %v", statusCode)
 		return
 	}
 
-	expectedBody := `{"message":"access denied","status":"error"}`
-	if body != expectedBody {
-		t.Errorf("Expected body to be %v, got %v", expectedBody, body)
+	var response types.UploadResponse
+	decoder := json.NewDecoder(bytes.NewReader([]byte(body)))
+	decoder.DisallowUnknownFields()
+	if err := decoder.Decode(&response); err != nil {
+		t.Errorf("Error unmarshalling response: %v", err)
+		return
+	}
+
+	if response.Url == "" {
+		t.Errorf("Expected URL to not be empty")
+		return
+	}
+
+	if !utils.IsURLStrict(response.Url) {
+		t.Errorf("Expected URL to be valid")
+		return
+	}
+
+	region := os.Getenv("AWS_REGION")
+	bucketName := os.Getenv("AWS_BUCKET_NAME")
+	folderName := os.Getenv("AWS_FOLDER_NAME")
+
+	if strings.Contains(response.Url, "https://"+storage.RemoveLeadingSlash(bucketName)+".s3."+region+".amazonaws.com/"+folderName+"/") == false {
+		t.Errorf("Invalid URL format:" + response.Url)
+		return
 	}
 }
 
